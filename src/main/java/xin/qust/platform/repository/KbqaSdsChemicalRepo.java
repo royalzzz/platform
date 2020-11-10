f95d5f32db017880cba27ca369571ee43ebeb24f
shulin.Acer <shulin.zou@qq.com>
2020-11-10 23:20:11 +0800

    标准树存入数据库，并前台展示

--- src/main/java/xin/qust/platform/repository/KbqaSdsChemicalRepo.java
+++
@@ -1,7 +0,0 @@
-package xin.qust.platform.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import xin.qust.platform.domain.KbqaSdsChemical;
-
-public interface KbqaSdsChemicalRepo extends JpaRepository<KbqaSdsChemical, Long> {
-}
--- src/main/java/xin/qust/platform/repository/UserRoleRepo.java
+++
@@ -1,7 +0,0 @@
-package xin.qust.platform.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import xin.qust.platform.domain.UserRole;
-
-public interface UserRoleRepo extends JpaRepository<UserRole, Long> {
-}
